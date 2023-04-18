603d0ecc552b711ecdae5c552ed8d19daf440bb3
hab <Mohabzax4@gmail.com>
2023-04-19 00:23:21 +0200

    Dependency injection

---
+++ lib/core/services/service_locator.dart
@@ -0,0 +1,23 @@
+import 'package:get_it/get_it.dart';
+import 'package:mova/data/repository/base_repository_impl.dart';
+import 'package:mova/data/source/remote_datasource.dart';
+import 'package:mova/domain/repository/base_repository.dart';
+import 'package:mova/domain/usecase/now_playing_usecase.dart';
+import 'package:mova/presentation/controllers/movie_bloc.dart';
+
+final getIt = GetIt.instance;
+
+class ServicesLocator {
+  void init() {
+    getIt.registerLazySingleton<DataSourceRepository>(
+        () => MovieRemoteDataSource());
+
+    getIt.registerLazySingleton<BaseRepository>(
+        () => BaseRepositoryImpl(getIt()));
+
+    getIt.registerLazySingleton(() => GetNowPlyingUseCase(getIt()));
+
+    // Bloc
+    getIt.registerFactory(() => MovieBloc(getNowPlayingUseCase: getIt()));
+  }
+}
