4d0b7d2543294928b50a1f12e72e5f4dd8d4bb2a
hab <Mohabzax4@gmail.com>
2023-04-18 15:11:39 +0200

    Constance

---
+++ lib/core/util/constance.dart
@@ -0,0 +1,5 @@
+class Api {
+  static const String BASE_URL = "https://api.themoviedb.org/3/movie";
+  static const String KEY = "?api_key=15cc2d06189e5b776834afe0bdef40ba";
+  static const String NOW_PLAYING_URL = "$BASE_URL/now_playing$KEY";
+}
