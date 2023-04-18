74a0d5b413498f2d775b1745c73ad543e7f84b94
hab <Mohabzax4@gmail.com>
2023-04-18 13:42:07 +0200

    initial project

---
+++ lib/main.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const Mova());
+}
+
+class Mova extends StatelessWidget {
+  const Mova({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
