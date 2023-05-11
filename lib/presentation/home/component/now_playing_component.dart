8e6c21681cb4ea119faaf3cc869ca31329e45b8f
hab <mohab.khalid.mahmoud@gmail.com>
2023-05-11 11:18:06 +0300

    Update now_playing_component.dart

--- lib/presentation/home/component/now_playing_component.dart
+++ lib/presentation/home/component/now_playing_component.dart
@@ -27,35 +27,61 @@ class NowPlayingComponent extends StatelessWidget {
           case RequestState.loaded:
             final movie = state.nowPlayingMovies;
             return CarouselSlider.builder(
-                itemCount: movie.length,
-                itemBuilder: (context, index, realIndex) {
-                  return GestureDetector(
-                    onTap: () {
-                      Navigator.push(
-                          context,
-                          MaterialPageRoute(
-                            builder: (context) => DetailPage(
-                                movieId: state.nowPlayingMovies[index].id),
-                          ));
-                    },
-                    child: CachedNetworkImage(
-                      width: MediaQuery.of(context).size.width,
-                      imageUrl: Network.imageUrl(movie[index].posterPath),
-                      fit: BoxFit.cover,
-                      placeholder: (context, url) => Shimmer.fromColors(
-                        baseColor: Colors.grey[300]!,
-                        highlightColor: Colors.grey[100]!,
-                        child: Container(
-                          color: Colors.transparent,
+              itemCount: movie.length,
+              itemBuilder: (context, index, realIndex) {
+                return GestureDetector(
+                  onTap: () {
+                    Navigator.push(
+                        context,
+                        MaterialPageRoute(
+                          builder: (context) =>
+                              DetailPage(movieId: movie[index].id),
+                        ));
+                  },
+                  child: Stack(
+                    children: [
+                      CachedNetworkImage(
+                        width: MediaQuery.of(context).size.width,
+                        imageUrl: Network.imageUrl(movie[index].backdropPath),
+                        fit: BoxFit.cover,
+                        height: 300.h,
+                        placeholder: (context, url) => Shimmer.fromColors(
+                          baseColor: Colors.grey[300]!,
+                          highlightColor: Colors.grey[100]!,
+                          child: Container(
+                            color: Colors.transparent,
+                          ),
+                        ),
+                        errorWidget: (context, url, error) => Center(
+                          child: Icon(Icons.error),
                         ),
                       ),
-                      errorWidget: (context, url, error) => Center(
-                        child: Icon(Icons.error),
+                      Positioned(
+                        bottom: 10.h,
+                        left: 16.w,
+                        child: Column(
+                          children: [
+                            Text(
+                              movie[index].title,
+                              style: Theme.of(context)
+                                  .textTheme
+                                  .displaySmall
+                                  ?.copyWith(color: Colors.white),
+                            ),
+                            Text(movie[index].genreIds.toString()),
+                          ],
+                        ),
                       ),
-                    ),
-                  );
-                },
-                options: CarouselOptions(height: 500.h, viewportFraction: 1));
+                    ],
+                  ),
+                );
+              },
+              options: CarouselOptions(
+                  height: 300.h,
+                  viewportFraction: 1,
+                  autoPlay: true,
+                  animateToClosest: true),
+            );
           case RequestState.error:
             return Center(
               child: Text("Error"),
